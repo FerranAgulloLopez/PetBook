d8066cfb5139000d29047ce851065c67ef6e29c7
Borja <cantero.aranda.borja@gmail.com>
2019-03-27 10:52:43 +0100

    Ventana eventos

--- PantallaFirstView/app/src/main/java/com/example/PETBook/MyEvents.java
+++ PantallaFirstView/app/src/main/java/com/example/PETBook/MyEvents.java
@@ -2,14 +2,20 @@ package com.example.PETBook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.pantallafirstview.R;
 
 public class MyEvents extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
