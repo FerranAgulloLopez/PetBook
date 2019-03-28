d7982ae9b385555a10fb978fcd50ccf5f0596a95
elmer13 <elmer_03_12@hotmail.com>
2019-03-28 20:12:21 +0100

    Registro mejorado

--- PantallaFirstView/app/src/main/java/com/example/PETBook/PantallaSignUp.java
+++ PantallaFirstView/app/src/main/java/com/example/PETBook/PantallaSignUp.java
@@ -1,6 +1,7 @@
 package com.example.PETBook;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,15 +14,16 @@ import android.app.DatePickerDialog;
 import java.text.SimpleDateFormat;
 import com.example.pantallafirstview.R;
 import java.util.Locale;
+import android.widget.Toast;
 
 public class PantallaSignUp extends AppCompatActivity {
 
-    TextView labelName;
-    TextView labelSurnames;
-    TextView labelMail;
-    TextView labelPassword1;
-    TextView labelPassword2;
-    TextView labelBirthday;
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputSurnames;
+    private TextInputLayout textInputMail;
+    private TextInputLayout textInputPassword1;
+    private TextInputLayout textInputPassword2;
+    private TextInputLayout textInputBirthday;
 
     private EditText inputName;
     private EditText inputSurnames;
@@ -39,6 +41,13 @@ public class PantallaSignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_sign_up);
 
+        textInputName      = (TextInputLayout) findViewById(R.id.nameTextInput);
+        textInputSurnames  = (TextInputLayout) findViewById(R.id.surnamesTextInput);
+        textInputMail      = (TextInputLayout) findViewById(R.id.mailTextInput);
+        textInputPassword1  = (TextInputLayout) findViewById(R.id.password1TextInput);
+        textInputPassword2 = (TextInputLayout) findViewById(R.id.password2TextInput);
+        textInputBirthday  = (TextInputLayout) findViewById(R.id.birthdayTextInput);
+
         inputName      = (EditText) findViewById(R.id.nameInput);
         inputSurnames  = (EditText) findViewById(R.id.surnamesInput);
         inputMail      = (EditText) findViewById(R.id.mailInput);
@@ -48,6 +57,7 @@ public class PantallaSignUp extends AppCompatActivity {
 
         buttonSignIn   = (Button) findViewById(R.id.signInButton);
 
+
         inputBirthday.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +66,15 @@ public class PantallaSignUp extends AppCompatActivity {
                         calendario.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+
 
     }
 
@@ -75,14 +94,82 @@ public class PantallaSignUp extends AppCompatActivity {
 
     private void actualizarInput() {
         String formatoDeFecha = "dd/mm/yy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(formatoDeFecha, Locale.US);
+        Locale spanish = new Locale("es", "ES");
+        SimpleDateFormat sdf = new SimpleDateFormat(formatoDeFecha, spanish);
 
         inputBirthday.setText(sdf.format(calendario.getTime()));
     }
 
-    public void signUp(View view){
-        Intent intent = new Intent(this, PantallaHome.class);
-        startActivity(intent);
+    private boolean validateName() {
+        if (inputName.getText().toString().isEmpty()) {
+            textInputName.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputName.setErrorEnabled(false);
+            return true;
+        }
+    }
 
+    private boolean validateSurnames() {
+        if (inputSurnames.getText().toString().isEmpty()) {
+            textInputSurnames.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputSurnames.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        if (inputMail.getText().toString().isEmpty()) {
+            textInputMail.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputMail.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePasswords() {
+        if (inputPassword1.getText().toString().isEmpty()) {
+            textInputPassword1.setError("Field can't be empty");
+            return false;
+        } else if (inputPassword1.getText().toString().trim().length() > 8 ) {
+            textInputPassword1.setError("Password too long");
+            return false;
+        } else {
+            textInputPassword1.setErrorEnabled(false);
+            return true;
+        }
+
+        if (inputPassword2.getText().toString().isEmpty()) {
+            textInputPassword2.setError("Field can't be empty");
+            return false;
+        } else if (inputPassword2.getText().toString().trim().length() > 8 ) {
+            textInputPassword2.setError("Password too long");
+            return false;
+        } else {
+            textInputPassword2.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+    private void signUp() {
+
+        if (validateName() && validateSurnames() && validateEmail() && validatePasswords()) {
+            /* recoger datos para backend
+
+            String name = inputName.getText().toString();
+            String surnames = inputSurnames.getText().toString();
+            String email = inputMail.getText().toString();
+            String password1 = inputPassword1.getText().toString();
+            String password2 = inputPassword2.getText().toString();
+            String birthday = inputPassword2.getText().toString();
+            */
+            Toast.makeText(this, "Registrado correctamente", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, PantallaLogSign.class);
+            startActivity(intent);
+        }
     }
 }
