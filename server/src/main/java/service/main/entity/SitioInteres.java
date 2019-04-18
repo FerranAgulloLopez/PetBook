e32297aeb10762f451c93c3bf9f194b9e422f5c8
Mohamed <cg3mbenali@gmail.com>
2019-04-18 16:31:45 +0200

    Clase SitioInteres + Repositorio añadidos

---
+++ server/src/main/java/service/main/entity/SitioInteres.java
@@ -0,0 +1,129 @@
+package service.main.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Document(collection = "SitioInteres")
+public class SitioInteres implements Serializable {
+
+    @Id
+    private String id;
+
+    private String nombre;
+    private String localizacion;
+
+    private String descripcion;
+    private String tipo;
+    private int numeroVotos;
+    private boolean aceptado;
+    private String emailCreador;
+
+
+
+    public SitioInteres() {
+        setNombre("NULL");
+        setLocalizacion("NULL");
+
+        makeId();
+    }
+
+    public SitioInteres(String nombre,
+                        String localizacion)
+    {
+
+        this.setLocalizacion("");
+        this.setNombre("");
+
+        setNombre(nombre);
+        setLocalizacion(localizacion);
+    }
+
+
+    public SitioInteres( String nombre,
+                         String localizacion,
+                         String descripcion,
+                         String tipo,
+                         int numeroVotos,
+                         boolean aceptado,
+                         String emailCreador)
+    {
+        this.setLocalizacion("");
+        this.setNombre("");
+
+
+        setNombre(nombre);
+        setLocalizacion(localizacion);
+        setDescripcion(descripcion);
+        setTipo(tipo);
+        setNumeroVotos(numeroVotos);
+        setAceptado(aceptado);
+        setEmailCreador(emailCreador);
+    }
+
+
+    private void makeId() {
+        id = nombre + localizacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        makeId();
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+        makeId();
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNumeroVotos() {
+        return numeroVotos;
+    }
+
+    public void setNumeroVotos(int numeroVotos) {
+        this.numeroVotos = numeroVotos;
+    }
+
+    public boolean isAceptado() {
+        return aceptado;
+    }
+
+    public void setAceptado(boolean aceptado) {
+        this.aceptado = aceptado;
+    }
+
+    public String getEmailCreador() {
+        return emailCreador;
+    }
+
+    public void setEmailCreador(String emailCreador) {
+        this.emailCreador = emailCreador;
+    }
+}
---
+++ server/src/main/java/service/main/repositories/SitioInteresRepository.java
@@ -0,0 +1,9 @@
+package service.main.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import service.main.entity.SitioInteres;
+
+public interface SitioInteresRepository extends MongoRepository<SitioInteres, String> {
+
+
+}
