c5bb84126bf394673765d36fa48e806ec18c821c
Ferran Agulló López <fewrnap@gmail.com>
2019-04-25 19:34:29 +0200

    adds mail key in a config file

--- server/src/main/java/service/main/util/SendEmailTLS.java
+++ server/src/main/java/service/main/util/SendEmailTLS.java
@@ -1,28 +1,33 @@
 package service.main.util;
 
+import org.json.JSONObject;
 import service.main.exception.InternalErrorException;
 
 import javax.mail.*;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class SendEmailTLS {
 
-    private static final String username = "petbookasesores@gmail.com";
-    private static final String password = "";
+    private static String pathConfigFile = "../config_files/mail_info.json";
+    private static String username;
+    private static String password;
 
     private static final Properties prop = new Properties();
 
-    public SendEmailTLS() {
+    public SendEmailTLS() throws InternalErrorException {
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        loadMailInfo();
     }
 
-    public void sendEmail(String mail, String subject, String content) throws InternalErrorException{
+    public void sendEmail(String mail, String subject, String content) throws InternalErrorException {
 
         try {
             Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
@@ -32,19 +37,39 @@ public class SendEmailTLS {
             });
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("from@gmail.com"));
+            message.setFrom(new InternetAddress(username));
             message.setRecipients(
                     Message.RecipientType.TO,
                     InternetAddress.parse(mail)
             );
             message.setSubject(subject);
             message.setText(content);
-
             Transport.send(message);
 
-            System.out.println("Done");
         } catch (Exception e) {
             throw new InternalErrorException("Error while sending a new email");
+        }
+    }
+
+    private void loadMailInfo() throws InternalErrorException {
+        try(FileReader fileReader = new FileReader(pathConfigFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            String data = "";
+
+            while((line = bufferedReader.readLine()) != null) {
+                data = data.concat(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(data);
+            username = jsonObject.getString("username");
+            password = jsonObject.getString("password");
+
+            if (username == null || password == null) throw new InternalErrorException("");
+
+        } catch (Exception e) {
+            throw new InternalErrorException("Error while loading email configuration file");
         }
 
     }
