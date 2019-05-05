0e68cc7e1542e4453e888f058b6f34bb2bbc6ebd
miguel.salamero <miguel.salamero@est.fib.upc.edu>
2019-05-05 12:47:00 +0200

    update ServerServiceImpl

--- server/src/main/java/service/main/service/ServerServiceImpl.java
+++ server/src/main/java/service/main/service/ServerServiceImpl.java
@@ -634,11 +634,12 @@ public class ServerServiceImpl implements ServerService {
 
 
         try {
+            String androidFcmKey="AAAAuaJvcA0:APA91bHRJoG3XEPJ3KR_xgbIzZV8CEhfwV_fdqGlpk3Af36nJW7Rklp6IgbPja89NpVR3GWUA9lXDzTpIFi71PWiUVzB_0cUEyPtE0K4Dg7nGjsx10-jMEQ6WjKM8RbZSV_Wmu0oZX0O";
             String androidFcmUrl="https://fcm.googleapis.com/fcm/send";
 
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders httpHeaders = new HttpHeaders();
-            //httpHeaders.set("Authorization", "key=" + androidFcmKey);
+            httpHeaders.set("Authorization", "key=" + androidFcmKey);
             httpHeaders.set("Content-Type", "application/json");
             JSONObject msg = new JSONObject();
             JSONObject json = new JSONObject();
